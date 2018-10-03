afd5489ad85145f13c821963f10afda1de058d00
Brian Farnhill <brfarn@amazon.com>
2018-10-03 14:45:15 +1000

    Using global URL for templates

--- build/publish.sh
+++ build/publish.sh
@@ -6,12 +6,12 @@ STACK_NAME="PlayerStartTemplate"
 aws cloudformation describe-stacks --stack-name $STACK_NAME >/dev/null 2>/dev/null
 if [ $? = 0 ]; then
     aws cloudformation update-stack --stack-name $STACK_NAME \
-                                    --template-url "https://s3-$REGION_NAME.amazonaws.com/$PUBLISH_BUCKET_NAME/cfn/player-start.yaml" \
+                                    --template-url "https://$PUBLISH_BUCKET_NAME.s3.amazonaws.com/cfn/player-start.yaml" \
                                     --capabilities CAPABILITY_IAM \
                                     --region $REGION_NAME
 else
     aws cloudformation create-stack --stack-name $STACK_NAME \
-                                    --template-url "https://s3-$REGION_NAME.amazonaws.com/$PUBLISH_BUCKET_NAME/cfn/player-start.yaml" \
+                                    --template-url "https://$PUBLISH_BUCKET_NAME.s3.amazonaws.com/cfn/player-start.yaml" \
                                     --capabilities CAPABILITY_IAM \
                                     --region $REGION_NAME
 fi
