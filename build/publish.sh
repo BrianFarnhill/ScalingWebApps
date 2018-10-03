45adb7029e772a8b0e412c24cffcba1d19205d84
Brian Farnhill <brfarn@amazon.com>
2018-10-03 14:33:10 +1000

    First attempt at automating deployment of cfn to us-west-2

--- build/publish.sh
+++ build/publish.sh
@@ -2,4 +2,16 @@
 
 aws s3 sync ./build/release "s3://$PUBLISH_BUCKET_NAME" --delete
 
-#TODO: Add the deployment of the cloudformation script for player start to here so we have it running somewhere
+STACK_NAME="PlayerStartTemplate"
+aws cloudformation describe-stacks --stack-name $STACK_NAME >/dev/null 2>/dev/null
+if [ $? = 0 ]; then
+    aws cloudformation update-stack --stack-name $STACK_NAME \
+                                    --template-url "https://s3-$REGION_NAME.amazonaws.com/$PUBLISH_BUCKET_NAME/cfn/player-start.yaml" \
+                                    --capabilities CAPABILITY_IAM \
+                                    --region $REGION_NAME
+else
+    aws cloudformation create-stack --stack-name $STACK_NAME \
+                                    --template-url "https://s3-$REGION_NAME.amazonaws.com/$PUBLISH_BUCKET_NAME/cfn/player-start.yaml" \
+                                    --capabilities CAPABILITY_IAM \
+                                    --region $REGION_NAME
+fi
