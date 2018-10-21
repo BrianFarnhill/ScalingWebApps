908a8af384c5e4fc9d5f2f7ac0a9f6982e06f7b3
Brian Farnhill <brfarn@amazon.com>
2018-10-21 11:17:37 +1100

    Got the load testing app to deploy through CFN

--- build/publish.sh
+++ build/publish.sh
@@ -3,7 +3,7 @@
 aws s3 sync ./build/release "s3://$PUBLISH_BUCKET_NAME" --delete
 
 STACK_NAME="PlayerStartTemplate"
-aws cloudformation describe-stacks --stack-name $STACK_NAME >/dev/null 2>/dev/null
+aws cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION_NAME >/dev/null 2>/dev/null
 if [ $? = 0 ]; then
     aws cloudformation update-stack --stack-name $STACK_NAME \
                                     --template-url "https://$PUBLISH_BUCKET_NAME.s3.amazonaws.com/cfn/player-start.yaml" \
